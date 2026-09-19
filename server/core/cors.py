5da261f9118e9e5d114bd3fedb97e53a63aa621c
Muhammadaziz <muhammadazizdev@gmail.com>
2026-09-19 15:16:05 +0600

    update

--- server/core/cors.py
+++ server/core/cors.py
@@ -16,6 +16,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     # front
+    "http://72.56.16.210",
+    "maximumcomfort.pro",
+    "*.maximumcomfort.pro",
+    # 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
